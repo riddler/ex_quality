67332999c2c2b3a0728e0e3438052f9ad730de3d
JohnnyT <ubergeek3141@gmail.com>
2026-01-09 05:06:14 -0700

    Adds initial Elixir project files (#2)

---
+++ lib/quality.ex
@@ -0,0 +1,18 @@
+defmodule Quality do
+  @moduledoc """
+  Documentation for `Quality`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Quality.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
